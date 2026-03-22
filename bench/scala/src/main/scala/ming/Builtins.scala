42e84e1d39f359a815c7fabd1dab31cfe2d2d0d9
my <my@famer.me>
2026-03-22 16:27:38 +0000

    checkpoint: L14 REGFIX (0s)

--- bench/scala/src/main/scala/ming/Builtins.scala
+++ bench/scala/src/main/scala/ming/Builtins.scala
@@ -93,8 +93,7 @@ object Builtins:
       case "string->symbol" => (stringToSymbol(args), "")
       case "string-ref"     => (stringRef(args), "")
       case "string-copy"    => (stringCopy(args), "")
-      case "string-set!" =>
-        throw new EvalError("string-set!: strings are immutable")
+      case "string-set!" => (stringSet(args), "")
       case "char?" => (typeCheck(name, args), "")
       case _       => BuiltinsL13.applyL13(name, args)
 
