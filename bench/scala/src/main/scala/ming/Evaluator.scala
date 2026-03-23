6b3ec8f59ea7a99ef3377d36857f2027bf23ccaf
my <my@famer.me>
2026-03-23 09:03:53 +0000

    checkpoint: L07 PASSED (144s)

--- bench/scala/src/main/scala/ming/Evaluator.scala
+++ bench/scala/src/main/scala/ming/Evaluator.scala
@@ -15,14 +15,14 @@ object Evaluator:
     val exprs = Parser.parseAll(input)
     if exprs.isEmpty then throw new EvalError("empty input")
     val env = makeGlobalEnv()
-    evalSequence(exprs, env).display
+    evalBody(exprs, env).display
 
   def evalStrWithOutput(input: String): (String, String) =
     val exprs = Parser.parseAll(input)
     if exprs.isEmpty then throw new EvalError("empty input")
     val output = new StringBuilder
     val env    = makeGlobalEnv(output)
-    val result = evalSequence(exprs, env)
+    val result = evalBody(exprs, env)
     (result.display, output.toString)
 
   private def makeGlobalEnv(output: StringBuilder = new StringBuilder): Env =
@@ -30,45 +30,163 @@ object Evaluator:
     Builtins.register(env, output)
     env
 
-  private def evalSequence(exprs: List[Expr], env: Env): Value =
+  /** Evaluate a sequence of expressions, returning the last value. Uses TCO for the last expr. */
+  private def evalBody(exprs: List[Expr], env: Env): Value =
     exprs match
       case Nil         => throw new EvalError("empty sequence")
       case last :: Nil => eval(last, env)
       case head :: tail =>
         eval(head, env)
-        evalSequence(tail, env)
+        evalBody(tail, env)
 
-  private def eval(expr: Expr, env: Env): Value = expr match
-    case Num(n, _)                            => IntVal(n)
-    case Bool(b, _)                           => BoolVal(b)
-    case Str(s, _)                            => StrVal(s.toCharArray)
-    case Chr(c, _)                            => CharVal(c)
-    case Sym(name, pos)                       => env.lookup(name, pos)
-    case SList(Nil, pos)                      => evalError("empty application", pos)
-    case SList(Sym("if", _) :: args, pos)     => evalIf(args, env, pos)
-    case SList(Sym("define", _) :: args, pos) => evalDefine(args, env, pos); BoolVal(true)
-    case SList(Sym("quote", _) :: args, pos)  => evalQuote(args, pos)
-    case SList(Sym("lambda", _) :: args, pos) => evalLambda(args, env, pos)
-    case SList(Sym("let", _) :: args, pos)    => evalLet(args, env, pos)
-    case SList(Sym("begin", _) :: args, pos)  => evalBegin(args, env, pos)
-    case SList(Sym("cond", _) :: args, pos)   => evalCond(args, env, pos)
-    case SList(Sym("and", _) :: args, _)      => evalAnd(args, env)
-    case SList(Sym("or", _) :: args, _)       => evalOr(args, env)
-    case SList(Sym("not", _) :: args, _)      => evalNot(args, env)
-    case SList(head :: args, pos) =>
-      val proc   = eval(head, env)
-      val values = args.map(eval(_, env))
-      applyProc(proc, values, pos)
+  /** Core evaluator with trampoline loop for tail-call optimization. */
+  private def eval(expr0: Expr, env0: Env): Value =
+    var curExpr: Expr = expr0
+    var curEnv: Env   = env0
 
-  private def evalIf(args: List[Expr], env: Env, pos: Option[Pos]): Value =
+    while true do
+      curExpr match
+        case Num(n, _)       => return IntVal(n)
+        case Bool(b, _)      => return BoolVal(b)
+        case Str(s, _)       => return StrVal(s.toCharArray)
+        case Chr(c, _)       => return CharVal(c)
+        case Sym(name, pos)  => return curEnv.lookup(name, pos)
+        case SList(Nil, pos) => evalError("empty application", pos)
+
+        case SList(Sym("if", _) :: args, pos) =>
+          args match
+            case cond :: thenBranch :: elseBranch :: Nil =>
+              if eval(cond, curEnv).isTruthy then curExpr = thenBranch
+              else curExpr = elseBranch
+            case cond :: thenBranch :: Nil =>
+              if eval(cond, curEnv).isTruthy then curExpr = thenBranch
+              else return BoolVal(false)
+            case _ => evalError("if: bad syntax", pos)
+
+        case SList(Sym("define", _) :: args, pos) =>
+          evalDefine(args, curEnv, pos)
+          return BoolVal(true)
+
+        case SList(Sym("quote", _) :: args, pos) =>
+          return evalQuote(args, pos)
+
+        case SList(Sym("lambda", _) :: args, pos) =>
+          return evalLambda(args, curEnv, pos)
+
+        case SList(Sym("let", _) :: args, pos) =>
+          val (nextExpr, nextEnv) = evalLetTco(args, curEnv, pos)
+          curExpr = nextExpr
+          curEnv = nextEnv
+
+        case SList(Sym("begin", _) :: args, pos) =>
+          if args.isEmpty then evalError("begin: empty", pos)
+          args.init.foreach(eval(_, curEnv))
+          curExpr = args.last
+
+        case SList(Sym("cond", _) :: clauses, pos) =>
+          evalCondTco(clauses, curEnv, pos) match
+            case Left(value) => return value
+            case Right((expr, env)) =>
+              curExpr = expr
+              curEnv = env
+
+        case SList(Sym("and", _) :: args, _) =>
+          args match
+            case Nil => return BoolVal(true)
+            case _ =>
+              var remaining = args
+              while remaining.tail.nonEmpty do
+                val v = eval(remaining.head, curEnv)
+                if !v.isTruthy then return v
+                remaining = remaining.tail
+              curExpr = remaining.head
+
+        case SList(Sym("or", _) :: args, _) =>
+          args match
+            case Nil => return BoolVal(false)
+            case _ =>
+              var remaining = args
+              while remaining.tail.nonEmpty do
+                val v = eval(remaining.head, curEnv)
+                if v.isTruthy then return v
+                remaining = remaining.tail
+              curExpr = remaining.head
+
+        case SList(Sym("not", _) :: args, _) =>
+          if args.length != 1 then throw new EvalError("not: expected 1 argument")
+          return BoolVal(!eval(args.head, curEnv).isTruthy)
+
+        case SList(head :: args, pos) =>
+          val proc   = eval(head, curEnv)
+          val values = args.map(eval(_, curEnv))
+          proc match
+            case LambdaVal(params, body, closure) =>
+              val localEnv = closure.extend(params, values)
+              body.init.foreach(eval(_, localEnv))
+              curExpr = body.last
+              curEnv = localEnv
+            case BuiltinVal(name, fn) =>
+              try return fn(values)
+              catch
+                case e: EvalError =>
+                  if e.getMessage.matches(".*\\d+:\\d+.*") then throw e
+                  else evalError(e.getMessage, pos)
+                case e: ArithmeticException =>
+                  evalError(e.getMessage, pos)
+            case _ => evalError("not a procedure", pos)
+    end while
+    throw new AssertionError("unreachable")
+
+  /** Find matching cond clause. Returns Left(value) for immediate result, or Right((expr, env)) for tail-position
+    * continuation.
+    */
+  private def evalCondTco(
+    clauses: List[Expr],
+    env: Env,
+    pos: Option[Pos]
+  ): Either[Value, (Expr, Env)] =
+    clauses match
+      case Nil => evalError("cond: no matching clause", pos)
+      case SList(Sym("else", _) :: body, _) :: Nil =>
+        body.init.foreach(eval(_, env))
+        Right((body.last, env))
+      case SList(test :: body, _) :: rest =>
+        if eval(test, env).isTruthy then
+          body.init.foreach(eval(_, env))
+          Right((body.last, env))
+        else evalCondTco(rest, env, pos)
+      case _ => evalError("cond: bad syntax", pos)
+
+  private def evalLetTco(
+    args: List[Expr],
+    env: Env,
+    pos: Option[Pos]
+  ): (Expr, Env) =
     args match
-      case cond :: thenBranch :: elseBranch :: Nil =>
-        if eval(cond, env).isTruthy then eval(thenBranch, env)
-        else eval(elseBranch, env)
-      case cond :: thenBranch :: Nil =>
-        if eval(cond, env).isTruthy then eval(thenBranch, env)
-        else BoolVal(false)
-      case _ => evalError("if: bad syntax", pos)
+      case Sym(name, _) :: SList(bindings, _) :: body if body.nonEmpty =>
+        val pairs = bindings.map {
+          case SList(Sym(v, _) :: valExpr :: Nil, _) => (v, eval(valExpr, env))
+          case _                                     => evalError("let: bad binding", pos)
+        }
+        val paramNames = pairs.map(_._1)
+        val initVals   = pairs.map(_._2)
+        val localEnv   = env.extend(Nil, Nil)
+        val lambda     = LambdaVal(paramNames, body, localEnv)
+        localEnv.define(name, lambda)
+        val callEnv = localEnv.extend(paramNames, initVals)
+        body.init.foreach(eval(_, callEnv))
+        (body.last, callEnv)
+
+      case SList(bindings, _) :: body if body.nonEmpty =>
+        val pairs = bindings.map {
+          case SList(Sym(v, _) :: valExpr :: Nil, _) => (v, eval(valExpr, env))
+          case _                                     => evalError("let: bad binding", pos)
+        }
+        val localEnv = env.extend(pairs.map(_._1), pairs.map(_._2))
+        body.init.foreach(eval(_, localEnv))
+        (body.last, localEnv)
+
+      case _ => evalError("let: bad syntax", pos)
 
   private def evalDefine(args: List[Expr], env: Env, pos: Option[Pos]): Unit =
     args match
@@ -107,74 +225,3 @@ object Evaluator:
         }
         LambdaVal(paramNames, body, env)
       case _ => evalError("lambda: bad syntax", pos)
-
-  private def evalLet(args: List[Expr], env: Env, pos: Option[Pos]): Value =
-    args match
-      case Sym(name, _) :: SList(bindings, _) :: body if body.nonEmpty =>
-        val pairs = bindings.map {
-          case SList(Sym(v, _) :: valExpr :: Nil, _) => (v, eval(valExpr, env))
-          case _                                     => evalError("let: bad binding", pos)
-        }
-        val paramNames = pairs.map(_._1)
-        val initVals   = pairs.map(_._2)
-        val localEnv   = env.extend(Nil, Nil)
-        val lambda     = LambdaVal(paramNames, body, localEnv)
-        localEnv.define(name, lambda)
-        applyProc(lambda, initVals, pos)
-      case SList(bindings, _) :: body if body.nonEmpty =>
-        val pairs = bindings.map {
-          case SList(Sym(v, _) :: valExpr :: Nil, _) => (v, eval(valExpr, env))
-          case _                                     => evalError("let: bad binding", pos)
-        }
-        val localEnv = env.extend(pairs.map(_._1), pairs.map(_._2))
-        evalSequence(body, localEnv)
-      case _ => evalError("let: bad syntax", pos)
-
-  private def evalBegin(args: List[Expr], env: Env, pos: Option[Pos]): Value =
-    if args.isEmpty then evalError("begin: empty", pos)
-    evalSequence(args, env)
-
-  private def evalCond(clauses: List[Expr], env: Env, pos: Option[Pos]): Value =
-    clauses match
-      case Nil => evalError("cond: no matching clause", pos)
-      case SList(Sym("else", _) :: body, _) :: Nil =>
-        evalSequence(body, env)
-      case SList(test :: body, _) :: rest =>
-        if eval(test, env).isTruthy then evalSequence(body, env)
-        else evalCond(rest, env, pos)
-      case _ => evalError("cond: bad syntax", pos)
-
-  private def evalAnd(args: List[Expr], env: Env): Value =
-    args match
-      case Nil         => BoolVal(true)
-      case last :: Nil => eval(last, env)
-      case head :: tail =>
-        val v = eval(head, env)
-        if !v.isTruthy then v else evalAnd(tail, env)
-
-  private def evalOr(args: List[Expr], env: Env): Value =
-    args match
-      case Nil         => BoolVal(false)
-      case last :: Nil => eval(last, env)
-      case head :: tail =>
-        val v = eval(head, env)
-        if v.isTruthy then v else evalOr(tail, env)
-
-  private def evalNot(args: List[Expr], env: Env): Value =
-    if args.length != 1 then throw new EvalError("not: expected 1 argument")
-    BoolVal(!eval(args.head, env).isTruthy)
-
-  private def applyProc(proc: Value, args: List[Value], pos: Option[Pos] = None): Value =
-    proc match
-      case LambdaVal(params, body, closure) =>
-        val localEnv = closure.extend(params, args)
-        evalSequence(body, localEnv)
-      case BuiltinVal(name, fn) =>
-        try fn(args)
-        catch
-          case e: EvalError =>
-            if e.getMessage.matches(".*\\d+:\\d+.*") then throw e
-            else evalError(e.getMessage, pos)
-          case e: ArithmeticException =>
-            evalError(e.getMessage, pos)
-      case _ => evalError("not a procedure", pos)
