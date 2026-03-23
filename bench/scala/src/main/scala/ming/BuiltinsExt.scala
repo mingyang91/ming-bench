ec2898996ac253647d9ee7a8f201eb2ef76c1503
my <my@famer.me>
2026-03-23 11:20:58 +0000

    checkpoint: L14 PASSED (140s)

--- bench/scala/src/main/scala/ming/BuiltinsExt.scala
+++ bench/scala/src/main/scala/ming/BuiltinsExt.scala
@@ -14,137 +14,140 @@ object BuiltinsExt:
     BuiltinsChar.register(env)
 
   private def registerStringOps(env: Env): Unit =
-    define(
-      env,
-      List(
-        (
-          "string-append",
-          args =>
-            val strs = args.map {
-              case StrVal(chars) => new String(chars)
-              case other         => throw new EvalError(s"string-append: not a string: ${other.display}")
-            }
-            StrVal(strs.mkString.toCharArray)
-        ),
-        (
-          "string-length",
-          args =>
-            if args.length != 1 then throw new EvalError("string-length: expected 1 argument")
-            args.head match
-              case StrVal(chars) => IntVal(chars.length.toLong)
-              case other         => throw new EvalError(s"string-length: not a string: ${other.display}")
-        ),
-        (
-          "substring",
-          args =>
-            args match
-              case StrVal(chars) :: IntVal(start) :: IntVal(end) :: Nil =>
-                StrVal(java.util.Arrays.copyOfRange(chars, start.toInt, end.toInt))
-              case _ => throw new EvalError("substring: expected (string, start, end)")
-        ),
-        (
-          "string->number",
-          args =>
-            if args.length != 1 then throw new EvalError("string->number: expected 1 argument")
-            args.head match
-              case StrVal(chars) =>
-                val s = new String(chars)
-                try IntVal(s.toLong)
-                catch case _: NumberFormatException => BoolVal(false)
-              case other => throw new EvalError(s"string->number: not a string: ${other.display}")
-        ),
-        (
-          "number->string",
-          args =>
-            if args.length != 1 then throw new EvalError("number->string: expected 1 argument")
-            args.head match
-              case IntVal(n) => StrVal(n.toString.toCharArray)
-              case other     => throw new EvalError(s"number->string: not a number: ${other.display}")
-        ),
-        (
-          "symbol->string",
-          args =>
-            if args.length != 1 then throw new EvalError("symbol->string: expected 1 argument")
-            args.head match
-              case SymbolVal(name) => StrVal(name.toCharArray)
-              case other           => throw new EvalError(s"symbol->string: not a symbol: ${other.display}")
-        ),
-        (
-          "string->symbol",
-          args =>
-            if args.length != 1 then throw new EvalError("string->symbol: expected 1 argument")
-            args.head match
-              case StrVal(chars) => SymbolVal(new String(chars))
-              case other         => throw new EvalError(s"string->symbol: not a string: ${other.display}")
-        ),
-        (
-          "string-ref",
-          args =>
-            args match
-              case StrVal(chars) :: IntVal(idx) :: Nil => CharVal(chars(idx.toInt))
-              case _                                   => throw new EvalError("string-ref: expected (string, index)")
-        ),
-        (
-          "string-copy",
-          args =>
-            if args.length != 1 then throw new EvalError("string-copy: expected 1 argument")
-            args.head match
-              case StrVal(chars) =>
-                val copy = chars.clone()
-                Value.markStringMutable(copy)
-                StrVal(copy)
-              case other => throw new EvalError(s"string-copy: not a string: ${other.display}")
-        ),
-        (
-          "string-set!",
-          args =>
-            if args.length != 3 then throw new EvalError("string-set!: expected 3 arguments")
-            args match
-              case StrVal(chars) :: IntVal(idx) :: CharVal(c) :: Nil =>
-                if !Value.isStringMutable(chars) then
-                  throw new EvalError("string-set!: strings are immutable")
-                val i = idx.toInt
-                if i < 0 || i >= chars.length then
-                  throw new EvalError(s"string-set!: index $i out of range [0, ${chars.length})")
-                chars(i) = c
-                NilVal
-              case _ => throw new EvalError("string-set!: expected (string, index, char)")
-        ),
-        (
-          "string->list",
-          args =>
-            if args.length != 1 then throw new EvalError("string->list: expected 1 argument")
-            args.head match
-              case StrVal(chars) =>
-                chars.foldRight(NilVal: Value)((c, acc) => PairVal(CharVal(c), acc))
-              case other => throw new EvalError(s"string->list: not a string: ${other.display}")
-        ),
-        (
-          "list->string",
-          args =>
-            if args.length != 1 then throw new EvalError("list->string: expected 1 argument")
-            val chars = EvalHelpers.valueToList(args.head).map {
-              case CharVal(c) => c
-              case other      => throw new EvalError(s"list->string: not a char: ${other.display}")
-            }
-            StrVal(chars.toArray)
-        ),
-        (
-          "char->integer",
-          args =>
-            if args.length != 1 then throw new EvalError("char->integer: expected 1 argument")
-            args.head match
-              case CharVal(c) => IntVal(c.toLong)
-              case _          => throw new EvalError("char->integer: not a char")
-        ),
-        (
-          "integer->char",
-          args =>
-            if args.length != 1 then throw new EvalError("integer->char: expected 1 argument")
-            args.head match
-              case IntVal(n) => CharVal(n.toChar)
-              case _         => throw new EvalError("integer->char: not a number")
-        )
+    define(env, stringConversionOps ++ stringMutationOps)
+
+  private def stringConversionOps: List[(String, List[Value] => Value)] =
+    List(
+      (
+        "string-append",
+        args =>
+          val strs = args.map {
+            case StrVal(chars) => new String(chars)
+            case other         => throw new EvalError(s"string-append: not a string: ${other.display}")
+          }
+          StrVal(strs.mkString.toCharArray)
+      ),
+      (
+        "string-length",
+        args =>
+          if args.length != 1 then throw new EvalError("string-length: expected 1 argument")
+          args.head match
+            case StrVal(chars) => IntVal(chars.length.toLong)
+            case other         => throw new EvalError(s"string-length: not a string: ${other.display}")
+      ),
+      (
+        "substring",
+        args =>
+          args match
+            case StrVal(chars) :: IntVal(start) :: IntVal(end) :: Nil =>
+              StrVal(java.util.Arrays.copyOfRange(chars, start.toInt, end.toInt))
+            case _ => throw new EvalError("substring: expected (string, start, end)")
+      ),
+      (
+        "string->number",
+        args =>
+          if args.length != 1 then throw new EvalError("string->number: expected 1 argument")
+          args.head match
+            case StrVal(chars) =>
+              val s = new String(chars)
+              try IntVal(s.toLong)
+              catch case _: NumberFormatException => BoolVal(false)
+            case other => throw new EvalError(s"string->number: not a string: ${other.display}")
+      ),
+      (
+        "number->string",
+        args =>
+          if args.length != 1 then throw new EvalError("number->string: expected 1 argument")
+          args.head match
+            case IntVal(n) => StrVal(n.toString.toCharArray)
+            case other     => throw new EvalError(s"number->string: not a number: ${other.display}")
+      ),
+      (
+        "symbol->string",
+        args =>
+          if args.length != 1 then throw new EvalError("symbol->string: expected 1 argument")
+          args.head match
+            case SymbolVal(name) => StrVal(name.toCharArray)
+            case other           => throw new EvalError(s"symbol->string: not a symbol: ${other.display}")
+      ),
+      (
+        "string->symbol",
+        args =>
+          if args.length != 1 then throw new EvalError("string->symbol: expected 1 argument")
+          args.head match
+            case StrVal(chars) => SymbolVal(new String(chars))
+            case other         => throw new EvalError(s"string->symbol: not a string: ${other.display}")
+      ),
+      (
+        "char->integer",
+        args =>
+          if args.length != 1 then throw new EvalError("char->integer: expected 1 argument")
+          args.head match
+            case CharVal(c) => IntVal(c.toLong)
+            case _          => throw new EvalError("char->integer: not a char")
+      ),
+      (
+        "integer->char",
+        args =>
+          if args.length != 1 then throw new EvalError("integer->char: expected 1 argument")
+          args.head match
+            case IntVal(n) => CharVal(n.toChar)
+            case _         => throw new EvalError("integer->char: not a number")
+      )
+    )
+
+  private def stringMutationOps: List[(String, List[Value] => Value)] =
+    List(
+      (
+        "string-ref",
+        args =>
+          args match
+            case StrVal(chars) :: IntVal(idx) :: Nil => CharVal(chars(idx.toInt))
+            case _                                   => throw new EvalError("string-ref: expected (string, index)")
+      ),
+      (
+        "string-copy",
+        args =>
+          if args.length != 1 then throw new EvalError("string-copy: expected 1 argument")
+          args.head match
+            case StrVal(chars) =>
+              val copy = chars.clone()
+              Value.markStringMutable(copy)
+              StrVal(copy)
+            case other => throw new EvalError(s"string-copy: not a string: ${other.display}")
+      ),
+      (
+        "string-set!",
+        args =>
+          if args.length != 3 then throw new EvalError("string-set!: expected 3 arguments")
+          args match
+            case StrVal(chars) :: IntVal(idx) :: CharVal(c) :: Nil =>
+              if !Value.isStringMutable(chars) then throw new EvalError("string-set!: strings are immutable")
+              val i = idx.toInt
+              if i < 0 || i >= chars.length then
+                throw new EvalError(s"string-set!: index $i out of range [0, ${chars.length})")
+              chars(i) = c
+              NilVal
+            case _ => throw new EvalError("string-set!: expected (string, index, char)")
+      ),
+      (
+        "string->list",
+        args =>
+          if args.length != 1 then throw new EvalError("string->list: expected 1 argument")
+          args.head match
+            case StrVal(chars) =>
+              chars.foldRight(NilVal: Value)((c, acc) => PairVal(CharVal(c), acc))
+            case other => throw new EvalError(s"string->list: not a string: ${other.display}")
+      ),
+      (
+        "list->string",
+        args =>
+          if args.length != 1 then throw new EvalError("list->string: expected 1 argument")
+          val chars = EvalHelpers.valueToList(args.head).map {
+            case CharVal(c) => c
+            case other      => throw new EvalError(s"list->string: not a char: ${other.display}")
+          }
+          StrVal(chars.toArray)
       )
     )
 
