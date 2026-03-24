b13f621a7f735a7e475b85d6586389c7b28ae8f5
my <my@famer.me>
2026-03-24 09:20:05 +0000

    checkpoint: L03 PASSED (133s)

---
+++ bench/scala/src/main/scala/ming/Builtins.scala
@@ -0,0 +1,181 @@
+package ming
+
+import scala.collection.mutable
+
+object Builtins:
+
+  private def requireNums(name: String, args: List[SchemeVal]): List[Long] =
+    args.map {
+      case SchemeVal.IntVal(n) => n
+      case other               => throw new EvalError(s"$name: expected number, got ${other.display}")
+    }
+
+  private def isTruthy(v: SchemeVal): Boolean = v match
+    case SchemeVal.BoolVal(false) => false
+    case _                        => true
+
+  private def numericCmp(
+    name: String,
+    op: (Long, Long) => Boolean
+  ): (String, SchemeVal) =
+    name -> SchemeVal.BuiltinProc(
+      name,
+      args =>
+        val nums = requireNums(name, args)
+        if nums.length < 2 then throw new EvalError(s"$name: expected at least 2 arguments")
+        SchemeVal.BoolVal(nums.sliding(2).forall(w => op(w(0), w(1))))
+    )
+
+  private def arithmeticBuiltins: List[(String, SchemeVal)] = List(
+    "+" -> SchemeVal.BuiltinProc(
+      "+",
+      args => SchemeVal.IntVal(requireNums("+", args).sum)
+    ),
+    "-" -> SchemeVal.BuiltinProc(
+      "-",
+      args =>
+        val nums = requireNums("-", args)
+        if nums.isEmpty then throw new EvalError("-: expected at least 1 argument")
+        if nums.length == 1 then SchemeVal.IntVal(-nums.head)
+        else SchemeVal.IntVal(nums.reduce(_ - _))
+    ),
+    "*" -> SchemeVal.BuiltinProc(
+      "*",
+      args => SchemeVal.IntVal(requireNums("*", args).product)
+    ),
+    "/" -> SchemeVal.BuiltinProc(
+      "/",
+      args =>
+        val nums = requireNums("/", args)
+        if nums.isEmpty then throw new EvalError("/: expected at least 1 argument")
+        if nums.length == 1 then SchemeVal.IntVal(1 / nums.head)
+        else
+          nums.tail.foreach(d => if d == 0 then throw new EvalError("division by zero"))
+          SchemeVal.IntVal(nums.reduce(_ / _))
+    )
+  )
+
+  private def comparisonBuiltins: List[(String, SchemeVal)] = List(
+    numericCmp("<", _ < _),
+    numericCmp(">", _ > _),
+    numericCmp("=", _ == _),
+    numericCmp("<=", _ <= _),
+    numericCmp(">=", _ >= _)
+  )
+
+  private def logicBuiltins: List[(String, SchemeVal)] = List(
+    "not" -> SchemeVal.BuiltinProc(
+      "not",
+      {
+        case List(arg) => SchemeVal.BoolVal(!isTruthy(arg))
+        case args =>
+          throw new EvalError(s"not: expected 1 argument, got ${args.length}")
+      }
+    )
+  )
+
+  private def listBuiltins: List[(String, SchemeVal)] = List(
+    "cons" -> SchemeVal.BuiltinProc(
+      "cons",
+      {
+        case List(a, SchemeVal.ListVal(elems)) => SchemeVal.ListVal(a :: elems)
+        case List(a, b)                        => SchemeVal.ListVal(List(a, b))
+        case args =>
+          throw new EvalError(s"cons: expected 2 arguments, got ${args.length}")
+      }
+    ),
+    "car" -> SchemeVal.BuiltinProc(
+      "car",
+      {
+        case List(SchemeVal.ListVal(h :: _)) => h
+        case List(SchemeVal.ListVal(Nil)) =>
+          throw new EvalError("car: empty list")
+        case List(other) =>
+          throw new EvalError(s"car: expected pair, got ${other.display}")
+        case args =>
+          throw new EvalError(s"car: expected 1 argument, got ${args.length}")
+      }
+    ),
+    "cdr" -> SchemeVal.BuiltinProc(
+      "cdr",
+      {
+        case List(SchemeVal.ListVal(_ :: t)) => SchemeVal.ListVal(t)
+        case List(SchemeVal.ListVal(Nil)) =>
+          throw new EvalError("cdr: empty list")
+        case List(other) =>
+          throw new EvalError(s"cdr: expected pair, got ${other.display}")
+        case args =>
+          throw new EvalError(s"cdr: expected 1 argument, got ${args.length}")
+      }
+    ),
+    "null?" -> SchemeVal.BuiltinProc(
+      "null?",
+      {
+        case List(SchemeVal.ListVal(Nil)) => SchemeVal.BoolVal(true)
+        case List(_)                      => SchemeVal.BoolVal(false)
+        case args =>
+          throw new EvalError(s"null?: expected 1 argument, got ${args.length}")
+      }
+    ),
+    "list" -> SchemeVal.BuiltinProc("list", args => SchemeVal.ListVal(args)),
+    "length" -> SchemeVal.BuiltinProc(
+      "length",
+      {
+        case List(SchemeVal.ListVal(elems)) =>
+          SchemeVal.IntVal(elems.length.toLong)
+        case List(other) =>
+          throw new EvalError(s"length: expected list, got ${other.display}")
+        case args =>
+          throw new EvalError(
+            s"length: expected 1 argument, got ${args.length}"
+          )
+      }
+    ),
+    "append" -> SchemeVal.BuiltinProc(
+      "append",
+      args =>
+        val lists = args.map {
+          case SchemeVal.ListVal(elems) => elems
+          case other =>
+            throw new EvalError(s"append: expected list, got ${other.display}")
+        }
+        SchemeVal.ListVal(lists.flatten)
+    )
+  )
+
+  private def typePredicate(
+    name: String,
+    test: SchemeVal => Boolean
+  ): (String, SchemeVal) =
+    name -> SchemeVal.BuiltinProc(
+      name,
+      {
+        case List(v) => SchemeVal.BoolVal(test(v))
+        case args =>
+          throw new EvalError(s"$name: expected 1 argument, got ${args.length}")
+      }
+    )
+
+  private def typePredicateBuiltins: List[(String, SchemeVal)] = List(
+    typePredicate("boolean?", _.isInstanceOf[SchemeVal.BoolVal]),
+    typePredicate("number?", _.isInstanceOf[SchemeVal.IntVal]),
+    typePredicate(
+      "pair?",
+      {
+        case SchemeVal.ListVal(_ :: _) => true
+        case _                         => false
+      }
+    ),
+    typePredicate("string?", _.isInstanceOf[SchemeVal.StrVal]),
+    typePredicate("symbol?", _.isInstanceOf[SchemeVal.SymVal])
+  )
+
+  def makeGlobalEnv(): Env =
+    val env = new Env(mutable.Map.empty, None)
+    val allBuiltins = arithmeticBuiltins
+      ++ comparisonBuiltins
+      ++ logicBuiltins
+      ++ listBuiltins
+      ++ typePredicateBuiltins
+    for (name, proc) <- allBuiltins do env.define(name, proc)
+    env
--- bench/scala/src/main/scala/ming/Evaluator.scala
+++ bench/scala/src/main/scala/ming/Evaluator.scala
@@ -30,7 +30,10 @@ enum SchemeVal:
     case BuiltinProc(name, _) => s"#<procedure:$name>"
     case Void                 => "#<void>"
 
-class Env(val bindings: mutable.Map[String, SchemeVal], val parent: Option[Env]):
+class Env(
+  val bindings: mutable.Map[String, SchemeVal],
+  val parent: Option[Env]
+):
 
   def lookup(name: String): SchemeVal =
     bindings.get(name) match
@@ -49,82 +52,15 @@ object Evaluator:
     case SchemeVal.BoolVal(false) => false
     case _                        => true
 
-  private def requireNums(name: String, args: List[SchemeVal]): List[Long] =
-    args.map {
-      case SchemeVal.IntVal(n) => n
-      case other               => throw new EvalError(s"$name: expected number, got ${other.display}")
-    }
-
-  private def numericCmp(name: String, op: (Long, Long) => Boolean): (String, SchemeVal) =
-    name -> SchemeVal.BuiltinProc(
-      name,
-      args =>
-        val nums = requireNums(name, args)
-        if nums.length < 2 then throw new EvalError(s"$name: expected at least 2 arguments")
-        SchemeVal.BoolVal(nums.sliding(2).forall(w => op(w(0), w(1))))
-    )
-
-  private def makeGlobalEnv(): Env =
-    val env = new Env(mutable.Map.empty, None)
-
-    env.define("+", SchemeVal.BuiltinProc("+", args => SchemeVal.IntVal(requireNums("+", args).sum)))
-
-    env.define(
-      "-",
-      SchemeVal.BuiltinProc(
-        "-",
-        args =>
-          val nums = requireNums("-", args)
-          if nums.isEmpty then throw new EvalError("-: expected at least 1 argument")
-          if nums.length == 1 then SchemeVal.IntVal(-nums.head)
-          else SchemeVal.IntVal(nums.reduce(_ - _))
-      )
-    )
-
-    env.define("*", SchemeVal.BuiltinProc("*", args => SchemeVal.IntVal(requireNums("*", args).product)))
-
-    env.define(
-      "/",
-      SchemeVal.BuiltinProc(
-        "/",
-        args =>
-          val nums = requireNums("/", args)
-          if nums.isEmpty then throw new EvalError("/: expected at least 1 argument")
-          if nums.length == 1 then SchemeVal.IntVal(1 / nums.head)
-          else
-            nums.tail.foreach(d => if d == 0 then throw new EvalError("division by zero"))
-            SchemeVal.IntVal(nums.reduce(_ / _))
-      )
-    )
-
-    for (name, proc) <- List(
-        numericCmp("<", _ < _),
-        numericCmp(">", _ > _),
-        numericCmp("=", _ == _),
-        numericCmp("<=", _ <= _),
-        numericCmp(">=", _ >= _)
-      )
-    do env.define(name, proc)
-
-    env.define(
-      "not",
-      SchemeVal.BuiltinProc(
-        "not",
-        {
-          case List(arg) => SchemeVal.BoolVal(!isTruthy(arg))
-          case args      => throw new EvalError(s"not: expected 1 argument, got ${args.length}")
-        }
-      )
-    )
-
-    env
-
   private def quoteToVal(expr: Expr): SchemeVal = expr match
     case Expr.IntLit(n)  => SchemeVal.IntVal(n)
     case Expr.BoolLit(b) => SchemeVal.BoolVal(b)
     case Expr.StrLit(s)  => SchemeVal.StrVal(s)
     case Expr.Symbol(n)  => SchemeVal.SymVal(n)
     case Expr.SList(es)  => SchemeVal.ListVal(es.map(quoteToVal))
+
+  private def evalBody(body: List[Expr], env: Env): SchemeVal =
+    body.foldLeft[SchemeVal](SchemeVal.Void)((_, e) => eval(e, env))
 
   private def eval(expr: Expr, env: Env): SchemeVal = expr match
     case Expr.IntLit(n)    => SchemeVal.IntVal(n)
@@ -135,42 +71,110 @@ object Evaluator:
     case Expr.SList(Expr.Symbol("quote") :: arg :: Nil) =>
       quoteToVal(arg)
     case Expr.SList(Expr.Symbol("if") :: cond :: thenBr :: elseBr :: Nil) =>
-      if isTruthy(eval(cond, env)) then eval(thenBr, env) else eval(elseBr, env)
+      if isTruthy(eval(cond, env)) then eval(thenBr, env)
+      else eval(elseBr, env)
     case Expr.SList(Expr.Symbol("if") :: cond :: thenBr :: Nil) =>
       if isTruthy(eval(cond, env)) then eval(thenBr, env) else SchemeVal.Void
-    case Expr.SList(Expr.Symbol("define") :: Expr.SList(Expr.Symbol(name) :: params) :: body) =>
+    case Expr.SList(
+          Expr.Symbol("define") :: Expr.SList(
+            Expr.Symbol(name) :: params
+          ) :: body
+        ) =>
       val paramNames = params.map {
-        case Expr.Symbol(n) => n; case _ => throw new EvalError("define: expected parameter name")
+        case Expr.Symbol(n) => n
+        case _              => throw new EvalError("define: expected parameter name")
       }
       env.define(name, SchemeVal.Procedure(paramNames, body, env))
       SchemeVal.Void
-    case Expr.SList(Expr.Symbol("define") :: Expr.Symbol(name) :: value :: Nil) =>
+    case Expr.SList(
+          Expr.Symbol("define") :: Expr.Symbol(name) :: value :: Nil
+        ) =>
       env.define(name, eval(value, env))
       SchemeVal.Void
     case Expr.SList(Expr.Symbol("lambda") :: Expr.SList(params) :: body) =>
       val paramNames = params.map {
-        case Expr.Symbol(n) => n; case _ => throw new EvalError("lambda: expected parameter name")
+        case Expr.Symbol(n) => n
+        case _              => throw new EvalError("lambda: expected parameter name")
       }
       SchemeVal.Procedure(paramNames, body, env)
+    case Expr.SList(
+          Expr.Symbol("let") :: Expr.Symbol(name) :: Expr.SList(
+            bindings
+          ) :: body
+        ) =>
+      evalNamedLet(name, bindings, body, env)
+    case Expr.SList(
+          Expr.Symbol("let") :: Expr.SList(bindings) :: body
+        ) =>
+      evalLet(bindings, body, env)
+    case Expr.SList(Expr.Symbol("begin") :: exprs) =>
+      evalBody(exprs, env)
+    case Expr.SList(Expr.Symbol("cond") :: clauses) =>
+      evalCond(clauses, env)
     case Expr.SList(Expr.Symbol("and") :: args) =>
       evalAnd(args, env)
     case Expr.SList(Expr.Symbol("or") :: args) =>
       evalOr(args, env)
     case Expr.SList(head :: args) =>
-      val fn         = eval(head, env)
-      val evaledArgs = args.map(a => eval(a, env))
-      fn match
-        case SchemeVal.BuiltinProc(_, f) => f(evaledArgs)
-        case SchemeVal.Procedure(params, body, closureEnv) =>
-          val newEnv = new Env(mutable.Map.empty, Some(closureEnv))
-          params.zip(evaledArgs).foreach((p, v) => newEnv.define(p, v))
-          var result: SchemeVal = SchemeVal.Void
-          for e <- body do result = eval(e, newEnv)
-          result
-        case other => throw new EvalError(s"not a procedure: ${other.display}")
+      applyProc(eval(head, env), args.map(a => eval(a, env)))
+
+  private def applyProc(fn: SchemeVal, evaledArgs: List[SchemeVal]): SchemeVal =
+    fn match
+      case SchemeVal.BuiltinProc(_, f) => f(evaledArgs)
+      case SchemeVal.Procedure(params, body, closureEnv) =>
+        val newEnv = new Env(mutable.Map.empty, Some(closureEnv))
+        params.zip(evaledArgs).foreach((p, v) => newEnv.define(p, v))
+        evalBody(body, newEnv)
+      case other => throw new EvalError(s"not a procedure: ${other.display}")
+
+  private def evalNamedLet(
+    name: String,
+    bindings: List[Expr],
+    body: List[Expr],
+    env: Env
+  ): SchemeVal =
+    val (paramNames, initExprs) = bindings.map {
+      case Expr.SList(Expr.Symbol(p) :: v :: Nil) => (p, v)
+      case _                                      => throw new EvalError("let: invalid binding")
+    }.unzip
+    val letEnv = new Env(mutable.Map.empty, Some(env))
+    val proc   = SchemeVal.Procedure(paramNames, body, letEnv)
+    letEnv.define(name, proc)
+    val initVals = initExprs.map(e => eval(e, env))
+    val callEnv  = new Env(mutable.Map.empty, Some(letEnv))
+    paramNames.zip(initVals).foreach((p, v) => callEnv.define(p, v))
+    evalBody(body, callEnv)
+
+  private def evalLet(
+    bindings: List[Expr],
+    body: List[Expr],
+    env: Env
+  ): SchemeVal =
+    val letEnv = new Env(mutable.Map.empty, Some(env))
+    for b <- bindings do
+      b match
+        case Expr.SList(Expr.Symbol(name) :: valExpr :: Nil) =>
+          letEnv.define(name, eval(valExpr, env))
+        case _ => throw new EvalError("let: invalid binding")
+    evalBody(body, letEnv)
 
   @scala.annotation.tailrec
-  private def evalAnd(args: List[Expr], env: Env, last: SchemeVal = SchemeVal.BoolVal(true)): SchemeVal =
+  private def evalCond(clauses: List[Expr], env: Env): SchemeVal =
+    clauses match
+      case Nil => SchemeVal.Void
+      case Expr.SList(Expr.Symbol("else") :: body) :: _ =>
+        evalBody(body, env)
+      case Expr.SList(test :: body) :: rest =>
+        if isTruthy(eval(test, env)) then evalBody(body, env)
+        else evalCond(rest, env)
+      case _ => throw new EvalError("cond: invalid clause")
+
+  @scala.annotation.tailrec
+  private def evalAnd(
+    args: List[Expr],
+    env: Env,
+    last: SchemeVal = SchemeVal.BoolVal(true)
+  ): SchemeVal =
     args match
       case Nil => last
       case head :: tail =>
@@ -179,7 +183,11 @@ object Evaluator:
         else evalAnd(tail, env, v)
 
   @scala.annotation.tailrec
-  private def evalOr(args: List[Expr], env: Env, last: SchemeVal = SchemeVal.BoolVal(false)): SchemeVal =
+  private def evalOr(
+    args: List[Expr],
+    env: Env,
+    last: SchemeVal = SchemeVal.BoolVal(false)
+  ): SchemeVal =
     args match
       case Nil => last
       case head :: tail =>
@@ -188,11 +196,9 @@ object Evaluator:
         else evalOr(tail, env, v)
 
   def evalStr(input: String): String =
-    val exprs             = Parser.parse(input)
-    val env               = makeGlobalEnv()
-    var result: SchemeVal = SchemeVal.Void
-    for expr <- exprs do result = eval(expr, env)
-    result.display
+    val exprs = Parser.parse(input)
+    val env   = Builtins.makeGlobalEnv()
+    evalBody(exprs, env).display
 
   def evalStrWithOutput(input: String): (String, String) =
     val result = evalStr(input)
