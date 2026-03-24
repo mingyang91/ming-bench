cb0c38076c7ea8a16b87cff9ff4ec8173da8a3f4
my <my@famer.me>
2026-03-24 09:35:37 +0000

    checkpoint: L07 PASSED (58s)

--- bench/scala/src/main/scala/ming/Evaluator.scala
+++ bench/scala/src/main/scala/ming/Evaluator.scala
@@ -61,6 +61,13 @@ class Env(
   def define(name: String, value: SchemeVal): Unit =
     bindings(name) = value
 
+  def set(name: String, value: SchemeVal): Unit =
+    if bindings.contains(name) then bindings(name) = value
+    else
+      parent match
+        case Some(p) => p.set(name, value)
+        case None    => throw new EvalError(s"set!: unbound variable: $name")
+
 object Evaluator:
 
   val outputBuffer: ThreadLocal[StringBuilder] = ThreadLocal.withInitial(() => new StringBuilder)
@@ -117,6 +124,11 @@ object Evaluator:
               Expr.Symbol("define") :: Expr.Symbol(name) :: value :: Nil
             ) =>
           env.define(name, eval(value, env))
+          SchemeVal.Void
+        case Expr.SList(
+              Expr.Symbol("set!") :: Expr.Symbol(name) :: value :: Nil
+            ) =>
+          env.set(name, eval(value, env))
           SchemeVal.Void
         case Expr.SList(Expr.Symbol("lambda") :: Expr.SList(params) :: body) =>
           val paramNames = params.map {
